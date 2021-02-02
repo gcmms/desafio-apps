376c42c4971bbe32e30bb931532021b73474f219
Gabriel Sanzone <gabriel.sanzone@gmail.com>
2021-02-02 00:29:15 -0300

    init

---
+++ OGlobo/OGlobo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OGlobo
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 02/02/21.
+//  Copyright © 2021 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OGlobo/OGloboTests/OGloboTests.swift
@@ -0,0 +1,34 @@
+//
+//  OGloboTests.swift
+//  OGloboTests
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 02/02/21.
+//  Copyright © 2021 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import XCTest
+@testable import OGlobo
+
+class OGloboTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
